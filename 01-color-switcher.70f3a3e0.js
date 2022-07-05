5cc433bc3275403b71d1f908473be683623cf8e1
SerhiyDukh1984 <100237254+SerhiyDukh1984@users.noreply.github.com>
2022-07-05 20:36:51 +0000

    Deploying to gh-pages from @ SerhiyDukh1984/goit-js-hw-09@cc184ec7653452bbcf47429e0c6221ea9a4a8198 🚀

--- 01-color-switcher.70f3a3e0.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){timeId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),500),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timeId),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.70f3a3e0.js.map
