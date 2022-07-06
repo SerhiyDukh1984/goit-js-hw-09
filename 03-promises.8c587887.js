12f814853790192e78c8bde5b852a055ff3937d2
SerhiyDukh1984 <100237254+SerhiyDukh1984@users.noreply.github.com>
2022-07-06 18:53:03 +0000

    Deploying to gh-pages from @ SerhiyDukh1984/goit-js-hw-09@aa0297c5f07bbc9210576a9abf5dce46c9c719a0 🚀

--- 03-promises.8c587887.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("delay");console.log("🚀 ~ FirstDelay",e);
-//# sourceMappingURL=03-promises.8c587887.js.map
--- 03-promises.bc2fd4e0.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("delay");console.log("🚀 ~ FirstDelay",e)}();
-//# sourceMappingURL=03-promises.bc2fd4e0.js.map
