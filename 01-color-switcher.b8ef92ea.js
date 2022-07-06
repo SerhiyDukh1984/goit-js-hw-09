f32c5738907e93e205ec8c79a2bcc5eba8e8bb81
SerhiyDukh1984 <100237254+SerhiyDukh1984@users.noreply.github.com>
2022-07-06 19:20:22 +0000

    Deploying to gh-pages from @ SerhiyDukh1984/goit-js-hw-09@88b9753b6817203bfd15ce71f6feb257c3c94a72 🚀

---
+++ 01-color-switcher.b8ef92ea.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){timeId=setInterval((function(){t.disabled=!0,e.disabled=!1,document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),500)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(timeId)}));e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.b8ef92ea.js.map
