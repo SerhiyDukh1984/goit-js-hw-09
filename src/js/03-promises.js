bd69f72d8e7bffb034163fa1d8e08a77d9b13535
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-07-06 23:53:47 +0300

    trying make function

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,41 +6,42 @@ const buttonEl = document.querySelector('button');
 
 formEl.addEventListener('submit', onSubmitClick);
 
+let counter = 0;
+
 function onSubmitClick(e) {
   buttonEl.disabled = true;
   e.preventDefault();
   let delay = Number(firstDelayEl.value);
 
   for (let i = 1; i <= amountEl.value; i += 1) {
-    createPromise(i, delay).then(onSuccess).catch(onError);
+    position = i;
+    createPromise(position, delay).then(resolve).catch(reject);
     delay += Number(stepEl.value);
   }
 }
 
-function onSuccess({ position, delay }) {
-  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-}
+function onSuccess({ position, delay }) {}
 function onError({ position, delay }) {
-  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  console.log();
 }
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     timeoutId = setTimeout(() => {
-      let position = 0;
-      let counter = 0;
       if (counter > amountEl) {
         clearTimeout(timeoutId);
         buttonEl.disabled = false;
         return;
       }
       const shouldResolve = Math.random() > 0.3;
-      position = +1;
+
       counter = +1;
       if (shouldResolve) {
-        resolve({ position, delay });
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        return { position, delay };
       } else {
-        reject({ position, delay });
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        return { position, delay };
       }
     }, delay);
   });
