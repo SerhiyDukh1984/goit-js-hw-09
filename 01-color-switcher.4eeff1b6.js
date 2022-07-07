42afb4259a8bd8bb7cefee18f09ce60ca0853afd
SerhiyDukh1984 <100237254+SerhiyDukh1984@users.noreply.github.com>
2022-07-07 05:27:37 +0000

    Deploying to gh-pages from @ SerhiyDukh1984/goit-js-hw-09@7e7e51a9677cc9dda12ccb0e1e7b854aa3b7cd1e 🚀

--- 01-color-switcher.4eeff1b6.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){return a=setInterval((function(){t.disabled=!0,e.disabled=!1,document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),500)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)})),e.disabled=!0;var a=0}();
-//# sourceMappingURL=01-color-switcher.4eeff1b6.js.map
--- 01-color-switcher.f92d0a05.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){return d=setInterval((()=>{e.disabled=!0,t.disabled=!1,document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),500),d})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(d)})),t.disabled=!0;let d=0;
-//# sourceMappingURL=01-color-switcher.f92d0a05.js.map
